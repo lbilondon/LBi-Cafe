19071dd56c1afa710e7af2fd6a8e0a5ac9f21356
Andy Davies <andy.davies@gmail.com>
2012-04-19 15:44:28 +0100

    Created application using Refinery CMS 2.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbicafe::Application.config.secret_token = '87f342a61608d1057a993116d73866fe9610c3b7afe176848a7c9962552ee91a10be1987d60a8d232c502b520b9e01395d5bec69de5f4a678d17d453b0ba51d7'
